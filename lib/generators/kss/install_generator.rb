430436adfa199a5a25ad1a1c5adcbecfc7abd678
Max Walker <max.walker@zweitag.de>
2012-04-17 10:51:16 +0200

    Revert "Add templates (categories: 'css', 'html', 'js', 'rb' + example style) and change install_generator accordingly"
    This reverts commit 4847e651913105d8b1316c2f541d8cba11b70dcb.
    
    Conflicts:
    
    	lib/generators/templates/controllers/css_controller.rb
    	lib/generators/templates/controllers/html_controller.rb
    	lib/generators/templates/controllers/js_controller.rb
    	lib/generators/templates/controllers/rb_controller.rb

--- lib/generators/kss/install_generator.rb
+++ lib/generators/kss/install_generator.rb
@@ -6,14 +6,10 @@ module Kss
     desc 'KSS Rails Install'
 
     def install_steps
-
-      directory 'controllers', 'app/controllers/kss'
-      directory 'views', 'app/views/kss'
-      directory 'layouts', 'app/views/layouts/kss'
-      directory 'stylesheets', 'app/assets/stylesheets'
-
-      route "mount Kss::Engine => '/styleguide' if Rails.env.development?"
-
+      copy_file 'index.html.erb', 'app/views/kss/home/index.html.erb'
+      copy_file 'styleguide.html.erb', 'app/views/kss/home/styleguide.html.erb'
+      copy_file 'application.html.erb', 'app/views/layouts/kss/application.html.erb'
+      route "mount Kss::Engine => '/kss' if Rails.env.development?"
     end
   end
 end
